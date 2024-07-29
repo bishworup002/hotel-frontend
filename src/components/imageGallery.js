eae5cf3643239d000e91370c5a7432b632f8c991
bishworup002 <bishworupm@gmail.com>
2024-07-29 15:01:52 +0600

    "Design change"

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import MainNav from "./mainNav";
 import hotel1 from "./image/hotel1.jpg";
 import hotel2 from "./image/hotel2.jpg";
@@ -11,7 +11,7 @@ import hotel8 from "./image/hotel8.jpg";
 import hotel9 from "./image/hotel9.jpg";
 import hotel10 from "./image/hotel10.jpg";
 import hotel11 from "./image/hotel11.jpg";
-
+import { Share, Heart, X, Copy, Mail, MessageCircle, Facebook, Twitter, Code } from 'lucide-react';
 import showAllIcon from "./image/showAll.png";
 import arrow from "./image/arrow.png";
 import share from "./image/share.png";
@@ -21,6 +21,9 @@ import love from "./image/love.png";
 function ImageGallery({hotel}) {
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600);
   const [isSaved, setIsSaved] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
+  const alertRef = useRef(null);
   const images = hotel?.images  ;
 
   const [modalOpen, setModalOpen] = useState(false);
@@ -55,6 +58,25 @@ function ImageGallery({hotel}) {
     });
   };
 
+  const handleShareClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCopyLinkClick = () => {
+    navigator.clipboard.writeText(window.location.href)
+      .then(() => {
+        setIsAlertVisible(true);
+        setTimeout(() => {
+          setIsAlertVisible(false);
+        }, 1000);
+      })
+      .catch((err) => console.error('Failed to copy: ', err));
+  };
+
   return (
     <div style={{ position: "relative" }}>
       {isSmallScreen && (
@@ -73,6 +95,7 @@ function ImageGallery({hotel}) {
               src={share}
               alt="Share"
               className="mobile-image-icon btn-share-mobile"
+              onClick={handleShareClick}
             />
             <img
               src={love}
@@ -83,6 +106,62 @@ function ImageGallery({hotel}) {
               onClick={handleSaveToggle}
             />
           </div>
+          {/* share modal */}
+          {isModalOpen && (
+        <div id="shareModal" className="shareModal">
+          <div className="shareModal-content">
+            <div className="shareModal-header">
+              <h2>Share this place</h2>
+            </div>
+            <span className="close" onClick={handleCloseClick}>
+              <X size={24} />
+            </span>
+
+            <div className="shareModal-body">
+              <div className="place-info">
+                <img src={hotel.images[0]} alt="Apartment" />
+                <p>{hotel.address} · ★New · 1 bedroom · {hotel.bedroom_count} beds · {hotel.bathroom_count} baths</p>
+              </div>
+              <div className="share-options">
+                <div className="share-option" onClick={handleCopyLinkClick}>
+                  <Copy size={24} />
+                  <p>Copy Link</p>
+                </div>
+                {isAlertVisible && (
+                  <div id="alert" ref={alertRef} className="alert">
+                    <span style={{ color: 'rgb(253, 255, 253)', backgroundColor: 'rgb(6, 153, 6)', borderRadius: '100% 24px' }}>&#x2713;</span>
+                    Link copied!
+                  </div>
+                )}
+                <div className="share-option">
+                  <Mail size={24} />
+                  <p>Email</p>
+                </div>
+                <div className="share-option">
+                  <MessageCircle size={24} />
+                  <p>Messages</p>
+                </div>
+                <div className="share-option">
+                  <MessageCircle size={24} />
+                  <p>Messenger</p>
+                </div>
+                <div className="share-option">
+                  <Facebook size={24} />
+                  <p>Facebook</p>
+                </div>
+                <div className="share-option">
+                  <Twitter size={24} />
+                  <p>Twitter</p>
+                </div>
+                <div className="share-option">
+                  <Code size={24} />
+                  <p>Embed</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
         </div>
       )}
 
@@ -188,5 +267,3 @@ const ModalImage = ({ isOpen, onClose,images }) => {
     </div>
   );
 };
-
-
